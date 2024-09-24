05edd629dd9e9e684f1fe5505d5c2457685ceeb9
Bogsch Balazs <bogsch.balazs@mkb.hu>
2024-09-24 10:17:56 +0200

    wordpicker v2 - with env

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 OCP Training 2024
+
+## Word Picker
+
+To run the word-picker API:
+```
+cd word-picker
+export FLASK_APP=word_picker.py
+/Users/bbogsch-mbh/Library/Python/3.11/bin/flask run
+```
+
+To build the Docker image:
+
+```
+docker build . -t word-picker:v*
+```
+
+
+To build the Docker image:
+
+```
+docker run -it --rm -p 5001:5000 -e OPTIONS_FILE=/tmp/missingfile.txt word-picker:v*
+```
--- word-picker/word_picker.py
+++ word-picker/word_picker.py
@@ -1,9 +1,16 @@
 from random import choice
 from flask import Flask
+from os import environ
 
 app = Flask(__name__)
+options_file = environ.get("OPTIONS_FILE","options.txt")
+def load_options(file_path):
+    with open(file_path,"r",encoding="utf-8") as f:
+        file_contents = f.read()
+        return file_contents.split()
 
-options = ["first", "second", "third"]
+options = load_options(options_file)
+# options = ["first", "second", "third"]
 
 @app.route("/")
 def pick_word():
